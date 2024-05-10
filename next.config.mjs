5ea4dfb339c600ec4b70f058f0a0a558e7642826
Mary <mary.pommer@tptlive.ee>
2024-05-10 16:44:49 +0300

    add yml file

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    basePath: "/recipePage",
     output: "export",
     images: { unoptimized: true }
-    //   images: {
-  //   remotePatterns: [
-  //     {
-  //       protocol: 'https',
-  //       hostname: 'www.themealdb.com',
-  //       port: '',
-  //       //pathname: '/images/category/**',
-  //     },
-  //   ],
-  // },
 };
 
 export default nextConfig;
